d78b988a93c8b0fd4b4c40e2a6d51dc957d69823
Alexandre <alexandreaanp@gmail.com>
2025-01-25 16:59:18 +0000

    Create next.js project

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import NavBar from "./components/navbar";
+
+
+
+export default function Home() {
+  return (
+   <> 
+    <NavBar name="eqw" dchangeTime="qwe"/>
+   </>
+  );
+}
