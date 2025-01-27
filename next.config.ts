3592162bf2a0fe7db16364fd95cff36bb970c9a4
Alexandre <alexandreaanp@gmail.com>
2025-01-27 22:22:59 +0000

    update

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   // Add valid config options here
   output: 'export',
-  basePath: '', // Set if your app will be served from a subdirectory
+  basePath: '/Monchimoveis-FE',
+  assetPrefix: '/Monchimoveis-FE/', // Set if your app will be served from a subdirectory
   trailingSlash: true,
   reactStrictMode: true,
 };
