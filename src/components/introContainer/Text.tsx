9dfa7a89a6ee7aa46727d4182ad1cf86034d4727
Alexandre <alexandreaanp@gmail.com>
2025-01-28 23:57:22 +0000

    added css into files

--- src/components/introContainer/Text.tsx
+++ src/components/introContainer/Text.tsx
@@ -1,8 +1,8 @@
 'use client';
-
-
 import IntroButton from "./Button"
 import React from "react";
+import styles from "./introContainer.module.css"
+
 
 export default function IntroText({title, subtitle, buttonContent, buttonHref}: {title:string, subtitle:string, buttonContent:string, buttonHref:string}) {
 
@@ -13,50 +13,23 @@ export default function IntroText({title, subtitle, buttonContent, buttonHref}: 
         const handleResize = () => {
           setIsMobile(window.innerWidth <= 768);
         };
-    
+        handleResize();
         window.addEventListener("resize", handleResize);
         return () => window.removeEventListener("resize", handleResize);
       }, []);
 
-    const divStyle = {
-        paddingTop: "300px",
-        paddingBottom: "200px",
-        // textAlign: "center",
-    }
-
-
-
-    const h1Style = {
-        color: "white",
-        fontSize: isMobile ? "40px" : "68px",
-        fontWeight: "400",
-        marginTop: "0",
-        marginBottom: "10px",
-        letterSpacing: "-1px",
-        lineHeight: "1.1"
-    }
-
-    const spanStyle = {
-        color: "#a7c44c",
-        fontWeight: 600
-    }
-
-    const pStyle = {
-        marginTop: "10px",
-        marginBottom: "40px",
-    }
 
     return (
-        <div className="container mx-auto text-center" style={divStyle} >
-            <h1 style={h1Style}>
+        <div className={`container mx-auto text-center ${styles.divStyle}`} >
+            <h1 className={styles.h1Style} style={isMobile ? {fontSize: "40px"} : {fontSize: "68px"}}>
                 
                 <strong>
                     {title}
                 </strong>
-                <span style={spanStyle}> / </span>
+                <span className={styles.spanStyle}> / </span>
                 {subtitle}
             </h1>
-            <p style={pStyle}></p>
+            <p className={styles.pStyle}></p>
             <IntroButton content={buttonContent} href={buttonHref}  />
         </div>
     )
